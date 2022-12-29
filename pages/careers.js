30333aed653f16334b507d5e16764da5dc96e2ac
WaleedGhori <waleedghori4@gmail.com>
2022-12-30 01:14:21 +0500

    Done

--- pages/careers.js
+++ pages/careers.js
@@ -3,7 +3,47 @@ import React from 'react'
 const careers = () => {
   return (
     <div>
-      
+      <section className='our_service mt-20 mb-12'>
+        <div className='container'>
+          <h2 class="text-xs text-indigo-500 tracking-widest font-medium text-center title-font mb-1">WEB TECHNOLOGY</h2>
+
+          <h2 className='font-bold text-3xl text-center mb-4'>Make`s Your Careers With Us</h2>
+          <hr className='w-20 h-1 bg-indigo-500 m-auto mt-0' />
+          <p className='mt-2 text-center m-12 font-serif'>Let’s boost your skill together</p>
+          <div className='flex justify-center items-center'>
+            <table class="table-auto border-solid border-2 border-indigo-200">
+              <thead className='bg-indigo-200'>
+                <tr>
+                  <th className='pl-7 pr-7'>Job Title</th>
+                  <th className='pl-7 pr-7'>Location</th>
+                </tr>
+              </thead>
+              <tbody className='pl-6 pr-6 border-solid border-2 border-indigo-200'>
+                <tr className='border-solid border-2 border-indigo-200'>
+                  <td className='pl-7 pr-7'>Mern Developer</td>
+                  <td className='pl-7 pr-7'>Karachi, Pakistan</td>
+                </tr>
+                <tr className='border-solid border-2 border-indigo-200'>
+                  <td className='pl-7 pr-7'>ASP.Net Developer</td>
+                  <td className='pl-7 pr-7'>Karachi, Pakistan</td>
+                </tr>
+                <tr className='border-solid border-2 border-indigo-200'>
+                  <td className='pl-7 pr-7'>Java Developer</td>
+                  <td className='pl-7 pr-7'>Karachi, Pakistan</td>
+                </tr>
+                <tr className='border-solid border-2 border-indigo-200'>
+                  <td className='pl-7 pr-7'>Machine Learning</td>
+                  <td className='pl-7 pr-7'>Karachi, Pakistan</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </section>
+      <section className='flex justify-center items-center mt-8 mb-8'>
+      <label className='font-semibold text-xl text-center mb-4'>Upload Your Resume Here: </label>
+      <input className='ml-2' type="file" id="myfile" name="myfile"/>
+      </section>
     </div>
   )
 }
--- pages/get_in_touch.js
+++ pages/get_in_touch.js
@@ -2,7 +2,18 @@ import React from 'react'
 
 const get_in_touch = () => {
   return (
-    <div>get_in_touch</div>
+    <div>
+      <section className='min-h-screen'>
+      <section className='our_service mt-20 mb-12'>
+          <div className='container'>
+            <h2 class="text-xs text-indigo-500 tracking-widest font-medium text-center title-font mb-1">WEB TECHNOLOGY</h2>
+
+            <h2 className='font-bold text-3xl text-center mb-4'>Get In Touch</h2>
+            <hr className='w-20 h-1 bg-indigo-500 m-auto mt-0' />
+          </div>
+        </section>
+      </section>
+    </div>
   )
 }
 
