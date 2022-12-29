5b7820c150c29dcf1584559f46c4d432acf87506
WaleedGhori <waleedghori4@gmail.com>
2022-12-30 02:22:11 +0500

    issues fix index of nav and foot

--- Components/Footer.js
+++ Components/Footer.js
@@ -23,21 +23,21 @@ const Footer = () => {
             <a className="text-gray-500">
               <svg
                 fill="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
                 className="w-5 h-5"
                 viewBox="0 0 24 24"
               >
                 <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
               </svg>
-            </a>
+            </a>Width
             <a className="ml-3 text-gray-500">
               <svg
                 fill="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
                 className="w-5 h-5"
                 viewBox="0 0 24 24"
               >
@@ -48,9 +48,9 @@ const Footer = () => {
               <svg
                 fill="none"
                 stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
                 className="w-5 h-5"
                 viewBox="0 0 24 24"
               >
@@ -62,9 +62,9 @@ const Footer = () => {
               <svg
                 fill="currentColor"
                 stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="0"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="0"
                 className="w-5 h-5"
                 viewBox="0 0 24 24"
               >
--- Components/Navbar.js
+++ Components/Navbar.js
@@ -1,16 +1,15 @@
 import React from 'react'
 import Link from 'next/link'
-import Image from "next/image";
 
 const Navbar = () => {
     return (
-        <header class="text-gray-600 body-font shadow-md">
-            <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-                <a class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-                    <span class="ml-3 text-xl text-indigo-600">WEB TECHNOLOGY</span>
+        <header className="text-gray-600 body-font shadow-md">
+            <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+                <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+                    <span className="ml-3 text-xl text-indigo-600">WEB TECHNOLOGY</span>
                 </a>
-                <nav class="md:ml-auto flex flex-wrap items-center text-base justify-center">
-                    <Link href={'/Home'} className="mr-5 hover:text-gray-900"><li className='list-none text-stone-900 font-sans'>Home</li></Link >
+                <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
+                    {/* <Link href={'/Home'} className="mr-5 hover:text-gray-900"><li className='list-none text-stone-900 font-sans'>Home</li></Link > */}
                     <Link href={'/about_us'} className="mr-5 hover:text-gray-900"><li className='list-none text-stone-900 font-sans'>About Us</li></Link >
                     <Link href={'/our_service'} className="mr-5 hover:text-gray-900"><li className='list-none text-stone-900 font-sans'>Services</li></Link >
                     <Link href={'/portfolio'} className="mr-5 hover:text-gray-900"><li className='list-none text-stone-900 font-sans'>Portfolio</li></Link >
@@ -18,7 +17,7 @@ const Navbar = () => {
                     <Link href={'/contact'} className="mr-5 hover:text-gray-900"><li className='list-none text-stone-900 font-sans'>Contact Us</li></Link >
                     <Link href={'/get_in_touch'} className="mr-5 hover:text-gray-900"><li className='list-none text-stone-900 font-sans'>Get in Touch</li></Link >
                 </nav>
-                <button class="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Here Blog
+                <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Here Blog
                 </button>
             </div>
         </header>
