c5b94b22f7f5e7ca93cca07bc1ca80183ec90df0
WaleedGhori <waleedghori4@gmail.com>
2023-01-01 23:28:45 +0500

    add link on web technology

--- Components/Navbar.js
+++ Components/Navbar.js
@@ -5,9 +5,9 @@ const Navbar = () => {
     return (
         <header className="text-gray-600 body-font shadow-md">
             <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-                <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+                <Link  href={'/'} className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
                     <span className="ml-3 text-xl text-indigo-600">WEB TECHNOLOGY</span>
-                </a>
+                </Link>
                 <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
                     {/* <Link href={'/Home'} className="mr-5 hover:text-gray-900"><li className='list-none text-stone-900 font-sans'>Home</li></Link > */}
                     <Link href={'/about_us'} className="mr-5 hover:text-gray-900"><li className='list-none text-stone-900 font-sans'>About Us</li></Link >
